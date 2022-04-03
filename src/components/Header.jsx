1fe7f2dfa837b724f00a0d3e6ab105b0b5e1a9f4
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-04-03 18:46:16 -0300

    Logo homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import Logo from "./img/fdlogo.jpg"
 
 function Header() {
     return (
-        <div className="fixed z-50 w-screen p-6 px16">
-           
+        <div className="fixed z-50 w-screen p-6 px16">           
            {/* desktop & tablet */}
                 <div className="hidden md:flex w-full h-full ">
-                    desktop
+                    <div className="flex items-center gap-2">
+                        <img src={Logo} className=" w-20  object-cover" alt="logo" />
+                        <p className="text-headingColor text-xl font-bold">
+                            dFood
+                        </p>
+                    </div>
                 </div>
 
            {/* mobile */}
