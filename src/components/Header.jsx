ea0593b02a79186dbe12d21f7e03cdcaa3f95298
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-04-03 19:16:59 -0300

    Change logo e add menu inicial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import Logo from "./img/fdlogo.jpg"
+import Logo from "./img/fddlogo.png"
 
 function Header() {
     return (
-        <div className="fixed z-50 w-screen p-6 px16">           
+        <header className="fixed z-50 w-screen p-6 px16">           
            {/* desktop & tablet */}
                 <div className="hidden md:flex w-full h-full ">
                     <div className="flex items-center gap-2">
-                        <img src={Logo} className=" w-20  object-cover" alt="logo" />
-                        <p className="text-headingColor text-xl font-bold">
+                        <img src={Logo} className=" w-24 object-cover" alt="logo" />
+                        <p className='text-headingColor text-xl font-bold'>
                             dFood
                         </p>
                     </div>
+
+                    <ul className="flex items-center gap-8 ml-auto">
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Quem somos</li>
+                        <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer'>Serviço</li>
+                        
+                    </ul>
+
                 </div>
 
            {/* mobile */}
@@ -19,7 +28,7 @@ function Header() {
                     mobile
                 </div>
 
-        </div>
+        </header>
     )
 }
 
