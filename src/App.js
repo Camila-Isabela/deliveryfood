e277577d0dca86f0e301327c55686c02484939e5
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-04-03 16:31:29 -0300

    Tailwind and classes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div className="w-screen h-screen flex items-center justify-center text-blue-600">
+            Apppp
+        </div>
+    )
+};
+
+export default App;
