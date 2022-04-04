d2fc1e44952f3589d9c3c64220e5e1a521e4d843
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-04-04 11:32:35 -0300

    Iniciando o user_authentication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Logo from "../img/fddlogo.png";
-import Avatar from "../img/avatar2.png"
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
 
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from '../firebase.config'
+
+import Logo from "../img/fddlogo.png";
+import Avatar from "../img/avatar2.png";
+import { Link } from "react-router-dom";
+
 const Header = () => {
+
+	const firebaseAuth = getAuth(app)
+	const provider = new GoogleAuthProvider()
+
+	const login = async () => {
+		const response = await signInWithPopup(firebaseAuth, provider)
+		console.log(response)
+	}
+
 	return (
 		<header className="fixed z-50 w-screen p-6 px-16">
 			{/* desktop & tablet */}
 			<div className="hidden md:flex w-full h-full items-center justify-between">
 				<Link to={"/"} className="flex items-center gap-2">
 					<img src={Logo} className=" w-20 object-cover" alt="logo" />
-					<p className="text-headingColor text-xl font-bold">
-						dFood
-					</p>
-				</Link> 
+					<p className="text-headingColor text-xl font-bold">dFood</p>
+				</Link>
 
 				<div className="flex items-center gap-8">
 					<ul className="flex items-center gap-8">
@@ -42,7 +53,15 @@ const Header = () => {
 						</div>
 					</div>
 
-                    <motion.img whileTap={{scale: 0.6}} src={Avatar} className="w-10 min-w[40px] h-10 min-h[40px] cursor-pointer" alt="Perfil do usuário" />
+					<div className="relative">
+						<motion.img
+							whileTap={{ scale: 0.6 }}
+							src={Avatar}
+							className="w-10 min-w[40px] h-10 min-h[40px] cursor-pointer"
+							alt="Perfil do usuário"
+							onClick={login}
+						/>
+					</div>
 
 				</div>
 			</div>
@@ -51,6 +70,6 @@ const Header = () => {
 			<div className="flex md:hidden w-full h-full ">mobile</div>
 		</header>
 	);
-}
+};
 
 export default Header;
