53f5ecc00182f34aa41b985a247135a48230ed0d
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-04-19 18:58:30 -0300

    adjusting login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,102 +11,101 @@ import { Link } from "react-router-dom";
 import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
 
-
-
 function Header() {
-    const firebaseAuth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+	const firebaseAuth = getAuth(app);
+	const provider = new GoogleAuthProvider();
 
-    const [{ user }, dispatch] = useStateValue();
+	const [{ user }, dispatch] = useStateValue();
 
-    const [isMenu, setIsMenu] = useState(false);
+	const [isMenu, setIsMenu] = useState(false);
 
-    async function login() {
-        if (!user) {
-            const {
-                // eslint-disable-next-line no-unused-vars
-                user: { refreshToken, providerData },
-            } = await signInWithPopup(firebaseAuth, provider);
-            dispatch({
-                type: actionType.SET_USER,
-                user: providerData[0],
-            });
-            localStorage.setItem("user", JSON.stringify(providerData[0]));
-        } else {
-            setIsMenu(!isMenu);
-        }
-    }
+	async function login() {
+		if (!user) {
+			const {
+				// eslint-disable-next-line no-unused-vars
+				user: { refreshToken, providerData },
+			} = await signInWithPopup(firebaseAuth, provider);
+			dispatch({
+				type: actionType.SET_USER,
+				user: providerData[0],
+			});
+			localStorage.setItem("user", JSON.stringify(providerData[0]));
+		} else {
+			setIsMenu(!isMenu);
+		}
+	}
 
-    return (
-        <header className="fixed z-50 w-screen p-6 px-16">
-            {/* desktop & tablet */}
-            <div className="hidden md:flex w-full h-full items-center justify-between">
-                <Link to={"/"} className="flex items-center gap-2">
-                    <img src={Logo} className=" w-20 object-cover" alt="logo" />
-                    <p className="text-headingColor text-xl font-bold">dFood</p>
-                </Link>
+	return (
+		<header className="fixed z-50 w-screen p-6 px-16">
+			{/* desktop & tablet */}
+			<div className="hidden md:flex w-full h-full items-center justify-between">
+				<Link to={"/"} className="flex items-center gap-2">
+					<img src={Logo} className=" w-20 object-cover" alt="logo" />
+					<p className="text-headingColor text-xl font-bold">dFood</p>
+				</Link>
 
-                <div className="flex items-center gap-8">
-                    <ul className="flex items-center gap-8">
-                        <li className="font-medium text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
-                            Home
-                        </li>
-                        <li className="font-medium text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
-                            Menu
-                        </li>
-                        <li className="font-medium text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
-                            Quem somos
-                        </li>
-                        <li className="font-medium text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
-                            Serviço
-                        </li>
-                    </ul>
+				<div className="flex items-center gap-8">
+					<ul className="flex items-center gap-8">
+						<li className="font-medium text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+							Home
+						</li>
+						<li className="font-medium text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+							Menu
+						</li>
+						<li className="font-medium text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+							Quem somos
+						</li>
+						<li className="font-medium text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+							Serviço
+						</li>
+					</ul>
 
-                    <div className="relative flex items-center justify-center">
-                        <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
-                        <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-activeText flex items-center justify-center">
-                            <p className="text-xs text-white font-semibold">
-                                2
-                            </p>
-                        </div>
-                    </div>
+					<div className="relative flex items-center justify-center">
+						<MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
+						<div className="absolute -top-2 -right-2 w-5 h-5 rounded-full bg-activeText flex items-center justify-center">
+							<p className="text-xs text-white font-semibold">
+								2
+							</p>
+						</div>
+					</div>
 
-                    <div className="relative">
-                        <motion.img
-                            whileTap={{ scale: 0.6 }}
-                            src={user ? user.photoURL : Avatar}
-                            className="w-10 min-w[40px] h-10 min-h[40px] cursor-pointer rounded-full"
-                            alt="User profile"
-                            onClick={login} />
-                        {isMenu && (
-                            <motion.div
-                                initial={{ opacity: 0, scale: 0.6 }}
-                                animate={{ opacity: 1, scale: 1 }}
-                                exit={{ opacity: 0, scale: 0.6 }}
-                                className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
-                            >
-                                {user &&
-                                    user.email ===
-                                    "camilaisabela26@gmail.com" && (
-                                        <Link to={"/createItem"}>
-                                            <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base ">
-                                                New Item <MdAdd />
-                                            </p>
-                                        </Link>
-                                    )}
-                                <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
-                                    Logout <MdLogout />
-                                </p>
-                            </motion.div>
-                        )}
-                    </div>
-                </div>
-            </div>
+					<div className="relative">
+						<motion.img
+							whileTap={{ scale: 0.6 }}
+							src={user ? user.photoURL : Avatar}
+							className="w-10 min-w[40px] h-10 min-h[40px] cursor-pointer rounded-full"
+							alt="User profile"
+							onClick={login}
+						/>
+						{isMenu && (
+							<motion.div
+								initial={{ opacity: 0, scale: 0.6 }}
+								animate={{ opacity: 1, scale: 1 }}
+								exit={{ opacity: 0, scale: 0.6 }}
+								className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
+							>
+								{user &&
+									user.email ===
+										"camila.1920478300067@faeterj-rio.edu.br" && (
+										<Link to={"/createItem"}>
+											<p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base ">
+												New Item <MdAdd />
+											</p>
+										</Link>
+									)}
+								<p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+									Logout <MdLogout />
+								</p>
+							</motion.div>
+						)}
+					</div>
+				</div>
+			</div>
 
-            {/* mobile */}
-            <div className="flex md:hidden w-full h-full ">mobile</div>
-        </header>
-    );
+			{/* mobile */}
+			<div className="flex md:hidden w-full h-full ">mobile</div>
+		</header>
+	);
 }
 
 export default Header;
