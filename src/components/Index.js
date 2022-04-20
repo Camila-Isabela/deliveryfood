f04e7a37e8b8ac8a999f39654c0688c2e8b87416
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-04-20 09:53:25 -0300

    fix deploy and names

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,3 +1,3 @@
-export { default as Header } from "./Header"
-export { default as MainContainer } from './MainContainer'
-export { default as CreateContainer } from './CreateContainer'
+export { default as Header } from "./Header";
+export { default as MainContainer } from './MainContainer';
+export { default as CreateContainer } from './CreateContainer';
