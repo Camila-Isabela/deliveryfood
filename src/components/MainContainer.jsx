872178dc7b15edf47f642e3fa900914222a4b033
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-04-20 12:37:52 -0300

    content container

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,7 +4,7 @@ import Delivery from "../img/fddlogo.png";
 const MainContainer = () => {
 	return (
 		<div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-			<div className="py-2 flex-1 flex flex-col items-start md:items-center justify-center gap-6">
+			<div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
 				<div className="flex items-center gap-2 justify-center bg-delBg px-4 py-1 rounded-full">
 					<p className="text-base text-green-700 font-semibold">
 						Bike Delivery
@@ -18,20 +18,27 @@ const MainContainer = () => {
 					</div>
 				</div>
 
-				<p className="text-[2.5rem] font-bold tracking-wide text-headingColor">
-					Delivery expresso em{" "}
-					<span className="text-green-600 text-[3rem]">
+				<p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+					A entrega mais rápida em{" "}
+					<span className="text-green-600 text-[3rem] lg:text-[5rem]">
 						sua cidade
 					</span>
 				</p>
 
-				<p className="text-base text-textColor text-center md:text-left">
+				<p className="text-base text-textColor text-center md:text-left md:w-[80%]">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
 					ipsam illo quasi dolorem adipisci assumenda ducimus
 					laudantium labore voluptates, dignissimos perferendis et
 					neque consequuntur nesciunt asperiores, cupiditate quod
 					culpa. Blanditiis!
 				</p>
+
+				<button
+					type="button"
+					className="bg-gradient-to-br from-green-400 to-green-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
+				>
+					Fazer Pedido
+				</button>
 			</div>
 			<div className="py-2 bg-blue-400 flex-1"></div>
 		</div>
