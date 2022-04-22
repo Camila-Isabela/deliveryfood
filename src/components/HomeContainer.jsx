c6c230b65c6cca0f25ca9d7fd125b14c5f8b8430
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-04-22 08:50:10 -0300

    Some adjusts in main container

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -3,7 +3,7 @@ import Delivery from "../img/fddlogo.png";
 
 const HomeContainer = () => {
 	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+		<section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
 			<div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
 				<div className="flex items-center gap-2 justify-center bg-delBg px-4 py-1 rounded-full">
 					<p className="text-base text-green-700 font-semibold">
@@ -18,14 +18,14 @@ const HomeContainer = () => {
 					</div>
 				</div>
 
-				<p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
+				<p className="text-[2.5rem] font-bold tracking-wide text-headingColor">
 					A entrega mais rápida em{" "}
-					<span className="text-green-600 text-[3rem] lg:text-[5rem]">
+					<span className="text-green-600 text-[2rem] lg:text-[3.5rem]">
 						sua cidade
 					</span>
 				</p>
 
-				<p className="text-base text-textColor text-center md:text-left md:w-[80%]">
+				<p className="text-base text-textColor text-center md:text-left md:w-[90%]">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
 					ipsam illo quasi dolorem adipisci assumenda ducimus
 					laudantium labore voluptates, dignissimos perferendis et
@@ -41,7 +41,7 @@ const HomeContainer = () => {
 				</button>
 			</div>
 			<div className="py-2 bg-blue-400 flex-1"></div>
-		</div>
+		</section>
 	);
 };
 
