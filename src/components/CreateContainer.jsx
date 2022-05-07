05580d96a12041ea1cae0e26c1c703c9f761e8f7
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-07 12:11:58 -0300

    settings tabs in vscode

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const CreateContainer = () => {
-  return (
-    <div>
-        CreateContainer
-    </div>
-  )
-}
+	return (
+		<div className="w-full min-h-screen flex items-center justify-center">
+			<div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center"></div>
+		</div>
+	);
+};
 
-export default CreateContainer
-
+export default CreateContainer;
