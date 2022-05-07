4f3f6ffb21aca673c2c6b269a7b7f5f86a498b02
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-07 11:19:39 -0300

    insert data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,7 +45,7 @@ function Header() {
 	}
 
 	return (
-		<header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
+		<header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
 			{/* desktop & tablet */}
 			<div className="hidden md:flex w-full h-full items-center justify-between">
 				<Link to={"/"} className="flex items-center gap-2">
