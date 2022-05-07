3dbe3fb41e25e63663f43c9f4301cc314da2c417
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-07 12:18:42 -0300

    create the states of items

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateContainer = () => {
+
+	const [title, setTitle] = useState("");
+	const [calories, setCalories] = useState("");
+	const [price, setPrice] = useState("");
+	const [category, setCategory] = useState("null");
+	const [fields, setFields] = useState("false");
+
 	return (
 		<div className="w-full min-h-screen flex items-center justify-center">
 			<div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center"></div>
