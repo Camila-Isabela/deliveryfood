b98e246f7f72b6601772c4d7126489bb4ccf7174
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-07 17:21:29 -0300

    upload image section

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { MdFastfood } from "react-icons/md";
+import { MdFastfood, MdCloudUpload, MdDelete } from "react-icons/md";
 import { categories } from "../utils/data";
+import Loader from "./Loader";
 
 const CreateContainer = () => {
 	const [title, setTitle] = useState("");
@@ -14,6 +15,10 @@ const CreateContainer = () => {
 	const [msg, setMsg] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
 	// const [{ foodItems }, dispatch] = useStateValue();
+
+	const uploadImage = () => {};
+
+	const deleteImage = () => {};
 
 	return (
 		<div className="w-full min-h-screen flex items-center justify-center">
@@ -40,29 +45,72 @@ const CreateContainer = () => {
 						required
 						value={title}
 						onChange={(e) => setTitle(e.target.value)}
-						placeholder="O que deseja pedir?"
+						placeholder="Insira o nome do produto"
 						className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor"
 					/>
 				</div>
 
 				<div className="w-full">
-					<select onChange={(e) => setCategory(e.target.value)} className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer">
+					<select
+						onChange={(e) => setCategory(e.target.value)}
+						className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+					>
 						<option value="other" className="bg-white">
 							Selecione a categoria
 						</option>
-						{categories && categories.map(item => (
-							<option key={item.id} className="text-base border-0 outline-none capitalize bg-white text-headingColor"
-							value={item.urlParamName}>
-								{item.name}
-							</option>
-						))}
+						{categories &&
+							categories.map((item) => (
+								<option
+									key={item.id}
+									className="text-base border-0 outline-none capitalize bg-white text-headingColor"
+									value={item.urlParamName}
+								>
+									{item.name}
+								</option>
+							))}
 					</select>
 				</div>
 
 				<div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg">
-						
-				</div>
+					{isLoading ? (
+						<Loader />
+					) : (
+						<>
+							{!imageAsset ? (
+								<>
+									<label className="w-full h-full flex flex-col items-center justify-center cursor-pointer">
+										<div className="w-full h-full flex flex-col items-center justify-center gap-2">
+											<MdCloudUpload className="text-gray-500 text-3xl hover:text-gray-700" />
+											<p className="text-gray-500 hover:text-gray-700">
+												Clique aqui para fazer upload
+											</p>
+										</div>
 
+										<input
+											type="file"
+											name="uploadimage"
+											accept="image/*"
+											onChange={uploadImage}
+											className="w-0 h-0"
+										/>
+									</label>
+								</>
+							) : (
+								<>
+									<div className="relative h-full">
+										<img
+											src={imageAsset}
+											alt="uploaded image"
+											className="w-full h-full object-cover"
+										/>
+										<button type="button" className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out" 
+										onClick={deleteImage}><MdDelete className="text-white" /></button>
+									</div>
+								</>
+							)}
+						</>
+					)}
+				</div>
 			</div>
 		</div>
 	);
