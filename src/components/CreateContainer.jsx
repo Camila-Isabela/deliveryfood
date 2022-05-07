47727731fe4dfb8e79df97470c7152f4f29922e7
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-07 16:45:48 -0300

    create items cart

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import {MdFastfood} from "react-icons/md"
+import { MdFastfood } from "react-icons/md";
+import { categories } from "../utils/data";
 
 const CreateContainer = () => {
 	const [title, setTitle] = useState("");
@@ -16,7 +17,7 @@ const CreateContainer = () => {
 
 	return (
 		<div className="w-full min-h-screen flex items-center justify-center">
-			<div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center">
+			<div className="w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
 				{fields && (
 					<motion.p
 						initial={{ opacity: 0 }}
@@ -33,15 +34,35 @@ const CreateContainer = () => {
 					</motion.p>
 				)}
 				<div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
-					<MdFastfood className="text-xl text-gray-700"/>
-					<input 
-					type="text" 
-					required value={title} 
-					onChange={(e) => setTitle(e.target.value)}
-					placeholder="Give me a title..."
-					className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor"
+					<MdFastfood className="text-xl text-gray-700" />
+					<input
+						type="text"
+						required
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+						placeholder="O que deseja pedir?"
+						className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor"
 					/>
 				</div>
+
+				<div className="w-full">
+					<select onChange={(e) => setCategory(e.target.value)} className="outline-none w-full text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer">
+						<option value="other" className="bg-white">
+							Selecione a categoria
+						</option>
+						{categories && categories.map(item => (
+							<option key={item.id} className="text-base border-0 outline-none capitalize bg-white text-headingColor"
+							value={item.urlParamName}>
+								{item.name}
+							</option>
+						))}
+					</select>
+				</div>
+
+				<div className="group flex justify-center items-center flex-col border-2 border-dotted border-gray-300 w-full h-225 md:h-420 cursor-pointer rounded-lg">
+						
+				</div>
+
 			</div>
 		</div>
 	);
