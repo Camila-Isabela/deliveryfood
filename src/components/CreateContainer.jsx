4d422d32af03616947906c17a2f7beba1085ac4e
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-07 18:35:38 -0300

    function upload with firebase

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from "react-icons/md";
+
+import {
+	MdFastfood,
+	MdCloudUpload,
+	MdDelete,
+	MdFoodBank,
+	MdAttachMoney,
+} from "react-icons/md";
+
 import { categories } from "../utils/data";
 import Loader from "./Loader";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { storage } from "../firebase.config";
 
 const CreateContainer = () => {
 	const [title, setTitle] = useState("");
@@ -17,8 +27,36 @@ const CreateContainer = () => {
 	// const [{ foodItems }, dispatch] = useStateValue();
 
 	const uploadImage = (e) => {
-		setIsLoading(true)
-		const imageFile = e.target.files[0]
+		setIsLoading(true);
+		const imageFile = e.target.files[0];
+		const storageRef = ref(
+			storage,
+			`Images/${Date.now()}-${imageFile.name}`
+		);
+		const uploadTask = uploadBytesResumable(storageRef, imageFile)
+		uploadTask.on('state_changed', (snapshot) => {
+			const uploadProgress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+		}, (error) => {
+			console.log(error)
+			setFields(true)
+			setMsg('Erro no upload : Tente novamente 🙇')
+			setAlertStatus('danger')
+			setTimeout(() => {
+				setFields(false)
+				setIsLoading(false)
+			}, 4000)
+		}, () => {
+			getDownloadURL(uploadTask.snapshot.ref).then(downloadURL => {
+				setImageAsset(downloadURL)
+				setIsLoading(false)
+				setFields(true)
+				setMsg('Upload realizado com sucesso! ☺')
+				setAlertStatus('success')
+				setTimeout(() => {
+					setFields(false)
+				}, 4000)
+			})
+		})
 	};
 
 	const deleteImage = () => {};
@@ -108,8 +146,13 @@ const CreateContainer = () => {
 											alt="uploaded image"
 											className="w-full h-full object-cover"
 										/>
-										<button type="button" className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out" 
-										onClick={deleteImage}><MdDelete className="text-white" /></button>
+										<button
+											type="button"
+											className="absolute bottom-3 right-3 p-3 rounded-full bg-red-500 text-xl cursor-pointer outline-none hover:shadow-md duration-500 transition-all ease-in-out"
+											onClick={deleteImage}
+										>
+											<MdDelete className="text-white" />
+										</button>
 									</div>
 								</>
 							)}
@@ -118,42 +161,45 @@ const CreateContainer = () => {
 				</div>
 
 				<div className="w-full flex flex-col md:flex-row items-center gap-3">
-
 					<div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
-						<MdFoodBank className="text-gray-700 text-2xl"/>
-						<input 
-							type="text" 
+						<MdFoodBank className="text-gray-700 text-2xl" />
+						<input
+							type="text"
 							required
 							value={calories}
-							onChange={(e) => setCalories(e.target.value) } 
-							placeholder="Calorias" 
+							onChange={(e) => setCalories(e.target.value)}
+							placeholder="Calorias"
 							className="w-full h-full text-lg 
 							bg-transparent outline-none 
-							border-none placeholder:text-gray-400 text-textColor" />
+							border-none placeholder:text-gray-400 text-textColor"
+						/>
 					</div>
 
 					<div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
-						<MdAttachMoney className="text-gray-700 text-2xl"/>
-						<input 
-							type="text" 
+						<MdAttachMoney className="text-gray-700 text-2xl" />
+						<input
+							type="text"
 							required
 							value={price}
-							onChange={(e) => setPrice(e.target.value)} 
-							placeholder="Preço" 
+							onChange={(e) => setPrice(e.target.value)}
+							placeholder="Preço"
 							className="w-full h-full text-lg 
 							bg-transparent outline-none 
-							border-none placeholder:text-gray-400 text-textColor" />
+							border-none placeholder:text-gray-400 text-textColor"
+						/>
 					</div>
-
 				</div>
 
 				<div className="flex items-center w-full">
-					<button 
-						type="button" 
+					<button
+						type="button"
 						className="ml-0 md:ml-auto w-full 
-						md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-white font-semibold" onClick={saveDetails}>Salvar</button>
+						md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-white font-semibold"
+						onClick={saveDetails}
+					>
+						Salvar
+					</button>
 				</div>
-
 			</div>
 		</div>
 	);
