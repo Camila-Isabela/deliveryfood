10e7d9d5561f53e240aa2e00586e26b8fc560967
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-07 17:55:29 -0300

    save details

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { MdFastfood, MdCloudUpload, MdDelete } from "react-icons/md";
+import { MdFastfood, MdCloudUpload, MdDelete, MdFoodBank, MdAttachMoney } from "react-icons/md";
 import { categories } from "../utils/data";
 import Loader from "./Loader";
 
@@ -19,6 +19,8 @@ const CreateContainer = () => {
 	const uploadImage = () => {};
 
 	const deleteImage = () => {};
+
+	const saveDetails = () => {};
 
 	return (
 		<div className="w-full min-h-screen flex items-center justify-center">
@@ -46,7 +48,7 @@ const CreateContainer = () => {
 						value={title}
 						onChange={(e) => setTitle(e.target.value)}
 						placeholder="Insira o nome do produto"
-						className="w-full h-full text-lg bg-transparent font-semibold outline-none border-none placeholder:text-gray-400 text-textColor"
+						className="w-full h-full text-lg bg-transparent  outline-none border-none placeholder:text-gray-400 text-textColor"
 					/>
 				</div>
 
@@ -111,6 +113,44 @@ const CreateContainer = () => {
 						</>
 					)}
 				</div>
+
+				<div className="w-full flex flex-col md:flex-row items-center gap-3">
+
+					<div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+						<MdFoodBank className="text-gray-700 text-2xl"/>
+						<input 
+							type="text" 
+							required
+							value={calories}
+							onChange={(e) => setCalories(e.target.value) } 
+							placeholder="Calorias" 
+							className="w-full h-full text-lg 
+							bg-transparent outline-none 
+							border-none placeholder:text-gray-400 text-textColor" />
+					</div>
+
+					<div className="w-full py-2 border-b border-gray-300 flex items-center gap-2">
+						<MdAttachMoney className="text-gray-700 text-2xl"/>
+						<input 
+							type="text" 
+							required
+							value={price}
+							onChange={(e) => setPrice(e.target.value)} 
+							placeholder="Preço" 
+							className="w-full h-full text-lg 
+							bg-transparent outline-none 
+							border-none placeholder:text-gray-400 text-textColor" />
+					</div>
+
+				</div>
+
+				<div className="flex items-center w-full">
+					<button 
+						type="button" 
+						className="ml-0 md:ml-auto w-full 
+						md:w-auto border-none outline-none bg-emerald-500 px-12 py-2 rounded-lg text-white font-semibold" onClick={saveDetails}>Salvar</button>
+				</div>
+
 			</div>
 		</div>
 	);
