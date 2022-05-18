541413982e24d6e4a7a8c99e32bb84ead3494d9f
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-05-18 11:02:03 -0300

    fetch data foodItems

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import { Route, Routes } from "react-router-dom";
-import { CreateContainer, Header, MainContainer } from './components'
+import { CreateContainer, Header, MainContainer } from "./components";
+import { useStateValue } from "./context/StateProvider";
+import { getAllFoodItems } from "./utils/firebaseFunctions";
+import { actionType } from "./context/reducer";
 
 const App = () => {
+	const [{}, dispatch] = useStateValue();
+
+	const fetchData = async () => {
+		await getAllFoodItems().then((data) => {
+			dispatch({
+				type: actionType.SET_FOOD_ITEMS,
+				foodItems: data
+			})
+		});
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
 	return (
 		<AnimatePresence exitBeforeEnter>
 			<div className="w-screen h-auto flex flex-col bg-primary">
-			<Header />
-			<main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full ">
-				<Routes>
-                    <Route path="/*" element={<MainContainer />}/>                   
-                    <Route path="/createItem" element={<CreateContainer />}/>                   
-                </Routes>
-			</main>
-		</div>
-		</AnimatePresence>		
+				<Header />
+				<main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full ">
+					<Routes>
+						<Route path="/*" element={<MainContainer />} />
+						<Route
+							path="/createItem"
+							element={<CreateContainer />}
+						/>
+					</Routes>
+				</main>
+			</div>
+		</AnimatePresence>
 	);
 };
 
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,19 +1,26 @@
 export const actionType = {
-    SET_USER : "SET_USER",
-}
-const reducer = (state, action) => {    
-    console.log(action)
+	SET_USER: "SET_USER",
+	SET_FOOD_ITEMS: "SET_FOOD_ITEMS",
+};
+const reducer = (state, action) => {
+	console.log(action);
 
-    switch(action.type) {
-        case actionType.SET_USER:
-            return {
-                ...state,
-                user: action.user,
-            }
-            
-        default:
-            return state
-    }
-}
+	switch (action.type) {
+		case actionType.SET_USER:
+			return {
+				...state,
+				user: action.user,
+			};
 
-export default reducer
+		case actionType.SET_FOOD_ITEMS:
+			return {
+				...state,
+				user: action.foodItems,
+			};
+
+		default:
+			return state;
+	}
+};
+
+export default reducer;
