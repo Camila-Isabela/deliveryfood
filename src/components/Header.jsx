12819f5787159392fe2039a231303da30735727c
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-05-18 10:42:17 -0300

    change user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -100,7 +100,7 @@ function Header() {
 							>
 								{user &&
 									user.email ===
-										"camila.isabela.ibm@gmail.com" && (
+										"camila.1920478300067@faeterj-rio.edu.br" && (
 										<Link to={"/createItem"}>
 											<p
 												className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
