f46e781919b29112d6abf737c63d3f0fa9305d81
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-05-20 09:52:30 -0300

    arrows change in carrousel images

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import HomeContainer from "./HomeContainer";
+import { motion } from "framer-motion";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const MainContainer = () => {
 	return (
@@ -7,20 +9,26 @@ const MainContainer = () => {
 			<HomeContainer />
 
 			<section className="w-full my-6">
-
 				<div className="w-full flex items-center justify-between">
-
-					 <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
+					<p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
 						Our fresh & healthy fruits
-					 </p>
+					</p>
 
 					<div className="hidden md:flex gap-3 items-center">
-						<div className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"></div>
-						<div className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"></div>
+						<motion.div
+							whileTap={{ scale: 0.75 }}
+							className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+						>
+							<MdChevronLeft className="text-lg text-white" />
+						</motion.div>
+						<motion.div
+							whileTap={{ scale: 0.75 }}
+							className="w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer transition-all duration-100 ease-in-out hover:shadow-lg flex items-center justify-center"
+						>
+							<MdChevronRight className="text-lg text-white" />
+						</motion.div>
 					</div>
-
 				</div>
-
 			</section>
 		</div>
 	);
