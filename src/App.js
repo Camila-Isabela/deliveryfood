b76ea7e9f1f4df0726dea49b00f56652ea57d004
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-05-21 13:37:02 -0300

    fix the packages and app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+/* eslint-disable */
+// eslint-disable-next-line react-hooks/exhaustive-deps
 import React, { useEffect } from "react";
-import { AnimatePresence } from "framer-motion";
 import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import { CreateContainer, Header, MainContainer } from "./components";
 import { useStateValue } from "./context/StateProvider";
 import { getAllFoodItems } from "./utils/firebaseFunctions";
 import { actionType } from "./context/reducer";
 
 const App = () => {
-	const [{}, dispatch] = useStateValue();
+	const [{foodItems}, dispatch] = useStateValue();
 
 	const fetchData = async () => {
 		await getAllFoodItems().then((data) => {
