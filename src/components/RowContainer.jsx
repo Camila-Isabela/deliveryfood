39cc9e51e8cf713b052541241652e3f1a9163985
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-22 08:29:51 -0300

    fix rowContainer

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,54 +1,93 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
+import NotFound from "../img/NotFound.svg";
+import { useStateValue } from "../context/StateProvider";
+import { actionType } from "../context/reducer";
 
-const RowContainer = ({ flag, data }) => {
-	console.log(data);
-	return (
-		<div
-			className={`w-full my-12 ${
-				flag ? "overflow-x-scroll" : "overflow-x-hidden"
-			}`}
-		>
-			{data &&
-				data.map((item) => (
-					<div key={item.id} className="w-300 md:w-340 h-auto bg-cardOverlay rounded-lg p-2 my-12 backdrop-blur-lg hover:drop-shadow-lg">
-						<div className="w-full flex items-center justify-between">
-							<motion.img
-								whileHover={{ scale: 1.2 }}
-								src="https://firebasestorage.googleapis.com/v0/b/deliveryfood-5dd22.appspot.com/o/Images%2F1653052281756-i6.png?alt=media&token=f3ab1733-32ba-46e0-bd67-32af52c0f54d"
-								alt=""
-								className="w-40 -mt-8 drop-shadow-2xl"
-							/>
+const RowContainer = ({ flag, data, scrollValue }) => {
+  const rowContainer = useRef();
 
-							<motion.div
-								whileTap={{ scale: 0.75 }}
-								className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md"
-							>
-								<MdShoppingBasket className="text-white" />
-							</motion.div>
-						</div>
+  const [items, setItems] = useState([]);
 
-						<div className="w-full flex flex-col items-end justify-end">
-							<p className="text-textColor font-semibold text-base md:text-lg">
-								Sundae
-							</p>
-							<p className="mt-1 text-sm text-gray-500">
-								120 calorias
-							</p>
-							<div className="flex items-center gap-8">
-								<p className="text-lg text-headingColor font-semibold">
-									<span className="text-sm text-red-500">
-										R${" "}
-									</span>
-									9.99
-								</p>
-							</div>
-						</div>
-					</div>
-				))}
-		</div>
-	);
+  const [{ cartItems }, dispatch] = useStateValue();
+
+  const addtocart = () => {
+    dispatch({
+      type: actionType.SET_CARTITEMS,
+      cartItems: items,
+    });
+    localStorage.setItem("cartItems", JSON.stringify(items));
+  };
+
+  useEffect(() => {
+    rowContainer.current.scrollLeft += scrollValue;
+  }, [scrollValue]);
+
+  useEffect(() => {
+    addtocart();
+  }, [items]);
+
+  return (
+    <div
+      ref={rowContainer}
+      className={`w-full flex items-center gap-3  my-12 scroll-smooth  ${
+        flag
+          ? "overflow-x-scroll scrollbar-none"
+          : "overflow-x-hidden flex-wrap justify-center"
+      }`}
+    >
+      {data && data.length > 0 ? (
+        data.map((item) => (
+          <div
+            key={item?.id}
+            className="w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative"
+          >
+            <div className="w-full flex items-center justify-between">
+              <motion.div
+                className="w-40 h-40 -mt-8 drop-shadow-2xl"
+                whileHover={{ scale: 1.2 }}
+              >
+                <img
+                  src={item?.imageURL}
+                  alt=""
+                  className="w-full h-full object-contain"
+                />
+              </motion.div>
+              <motion.div
+                whileTap={{ scale: 0.75 }}
+                className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md -mt-8"
+                onClick={() => setItems([...cartItems, item])}
+              >
+                <MdShoppingBasket className="text-white" />
+              </motion.div>
+            </div>
+
+            <div className="w-full flex flex-col items-end justify-end -mt-8">
+              <p className="text-textColor font-semibold text-base md:text-lg">
+                {item?.title}
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                {item?.calories} Calorias
+              </p>
+              <div className="flex items-center gap-8">
+                <p className="text-lg text-headingColor font-semibold">
+                  <span className="text-sm text-red-500">$</span> {item?.price}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div className="w-full flex flex-col items-center justify-center">
+          <img src={NotFound} className="h-340" />
+          <p className="text-xl text-headingColor font-semibold my-2">
+            Items não encontrados
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RowContainer;
