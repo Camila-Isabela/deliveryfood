49cc7a97c9f82e3d2f0bc50944f2c136490aa41a
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-22 08:37:05 -0300

    fetch cart and change dataItems

--- src/utils/fetchLocalStorageData.js
+++ src/utils/fetchLocalStorageData.js
@@ -6,3 +6,13 @@ export const fetchUser = () => {
 
 	return userInfo;
 };
+
+export const fetchCart = () => {
+	const cartInfo =
+	  localStorage.getItem("cartItems") !== "undefined"
+		? JSON.parse(localStorage.getItem("cartItems"))
+		: localStorage.clear();
+  
+	return cartInfo ? cartInfo : [];
+  };
+  
