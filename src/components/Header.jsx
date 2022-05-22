29e584ae07105e92748547e8eb21c3a3504b14a2
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-05-22 07:41:59 -0300

    modify user in other branch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -100,7 +100,7 @@ function Header() {
 							>
 								{user &&
 									user.email ===
-										"camila.isabela.ibm@gmail.com" && (
+										"camilaisabela26@gmail.com" && (
 										<Link to={"/createItem"}>
 											<p
 												className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
