9d25c017d651c468f5ff4d23d1cc8cd6e140becd
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-05-28 11:26:20 -0300

    change menu

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -16,7 +16,7 @@ const MenuContainer = () => {
     <section className="w-full my-6" id="menu">
       <div className="w-full flex flex-col items-center justify-center">
         <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-16 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 mr-auto">
-          Produtos
+          Menu
         </p>
 
         <div className="w-full flex items-center justify-start lg:justify-center gap-8 py-6 overflow-x-scroll scrollbar-none">
