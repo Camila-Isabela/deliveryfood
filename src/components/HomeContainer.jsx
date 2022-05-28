17fcd7e12693e5c6b8ec205a41cb496d90cbe080
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2022-05-28 11:23:44 -0300

    disability button unused

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -37,12 +37,12 @@ const HomeContainer = () => {
           suscipit!
         </p>
 
-        <button
+        {/* <button
           type="button"
           className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
         >
           Fazer pedido
-        </button>
+        </button> */}
       </div>
       <div className="py-2 flex-1 flex items-center relative">
         <img
