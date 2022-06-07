05b29feaea1efc00ff26f8fb5e5a2404e431e55f
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-06-07 09:53:32 -0300

    change validation login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -114,7 +114,7 @@ const Header = () => {
 							>
 								{/*Changing the way of login to any login */}
 								{/* user.email === "camilaisabela26@gmail.com" && */}
-								{user && (
+								{user && user.email === "camilaisabela26@gmail.com" ? (
 										<Link to={"/createItem"}>
 											<p
 												className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
@@ -122,14 +122,25 @@ const Header = () => {
 											>
 												New Item <MdAdd />
 											</p>
-										</Link>
-									)}
-								<p
+											<p
 									className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
 									onClick={logout}
 								>
 									Logout <MdLogout />
 								</p>
+										</Link>
+									) : (<p
+										className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+										onClick={logout}
+									>
+										Logout <MdLogout />
+									</p>)}
+								{/* <p
+									className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+									onClick={logout}
+								>
+									Logout <MdLogout />
+								</p> */}
 							</motion.div>
 						)}
 					</div>
