d73f888dc5b2526415b02b7ea9de653b306db000
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-06-07 10:22:24 -0300

    try to fix validation login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -93,7 +93,7 @@ const Header = () => {
 								className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
 							>
 								{user &&
-								user.email === "camilaisabela26@gmail.com" ? (
+								user.email === "camila@gmail.com" ? (
 									<Link to={"/createItem"}>
 										<p
 											className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
