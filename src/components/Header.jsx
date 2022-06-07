d2931ef7e66db32376043f0270328880725454b0
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-06-07 10:46:05 -0300

    ohhh yes! validation is right! 😎☺

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -93,14 +93,20 @@ const Header = () => {
 								className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
 							>
 								{user &&
-								user.email === "camila@gmail.com" ? (
-									<Link to={"/createItem"}>
-										<p
-											className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
-											onClick={() => setIsMenu(false)}
-										>
-											Novo Item <MdAdd />
-										</p>
+									user.email ===
+										"camilaisabela26@gmail.com" && (
+										<Link to={"/createItem"}>
+											<p
+												className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+												onClick={() => setIsMenu(false)}
+											>
+												Novo Item <MdAdd />
+											</p>
+
+										</Link>
+									)}
+								{user && (
+									<Link to={"/"}>
 										<p
 											className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
 											onClick={logout}
@@ -108,13 +114,6 @@ const Header = () => {
 											Logout <MdLogout />
 										</p>
 									</Link>
-								) : (
-									<p
-										className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
-										onClick={logout}
-									>
-										Logout <MdLogout />
-									</p>
 								)}
 							</motion.div>
 						)}
