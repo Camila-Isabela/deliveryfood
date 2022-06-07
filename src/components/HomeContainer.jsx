34ad61beed22b1ee405d80bebd4b783084ee4420
Camila Isabela Lins Soares <camila.isabela@ibm.com>
2022-06-07 10:07:29 -0300

    cleanCode HomePage

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import HomeBg from "../img/moto1.jpg";
-// import { heroData } from "../utils/data";
 
 const HomeContainer = () => {
   return (
@@ -9,19 +8,6 @@ const HomeContainer = () => {
       id="home"
     >
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
-        {/* <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
-          <p className="text-base text-orange-500 font-semibold">
-            Bike Delivery
-          </p>
-          <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
-            <img
-              src={Delivery}
-              className="w-full h-full object-contain"
-              alt="delivery"
-            />
-          </div>
-        </div> */}
-
         <p className="text-[2.5rem] lg:text-[4.5rem] font-bold tracking-wide text-headingColor">
           O Delivery mais rápido em{" "}
           <span className="text-orange-600 text-[3rem] lg:text-[5rem]">
@@ -32,13 +18,6 @@ const HomeContainer = () => {
         <p className="text-base text-textColor text-center md:text-left md:w-[80%]">
           Os melhores pedidos no melhor tempo! 😋
         </p>
-
-        {/* <button
-          type="button"
-          className="bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2  rounded-lg hover:shadow-lg transition-all ease-in-out duration-100"
-        >
-          Fazer pedido
-        </button> */}
       </div>
       <div className="py-2 flex-1 flex items-center relative">
         <img
@@ -46,33 +25,6 @@ const HomeContainer = () => {
           className=" ml-auto h-320 w-full lg:w-auto lg:h-420"
           alt="hero-bg"
         />
-        
-        {/* <div className="w-full h-full absolute top-0 left-0 flex flex-col items-center justify-center lg:px-16  py-4 gap-8 flex-wrap">
-          {heroData &&
-            heroData.map((n) => (
-              <div
-                key={n.id}
-                className="  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
-              >
-                <img
-                  src={n.imageSrc}
-                  className="w-20 lg:w-40 -mt-10 lg:-mt-30 "
-                  alt="I1"
-                />
-                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
-                  {n.name}
-                </p>
-
-                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
-                  {n.decp}
-                </p>
-
-                <p className="text-sm font-semibold text-headingColor">
-                  <span className="text-xs text-red-600">R$</span> {n.price}
-                </p>
-              </div>
-            ))}
-        </div> */}
       </div>
     </section>
   );
